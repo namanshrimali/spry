d8ab430fca31faad7b12c661bcebaddae26bfec4
Naman Shrimali <namanshrimali@gmail.com>
2020-03-09 00:18:14 +0530

    added child routes for the dashboard component

--- ui/spry-app/src/app/app-routing.module.ts
+++ ui/spry-app/src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FacadeComponent } from './dashboard/facade/facade.component';
+import { BoardsComponent } from './dashboard/boards/boards.component';
+import { HomeComponent } from './dashboard/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FacadeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'products',
+        component: BoardsComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

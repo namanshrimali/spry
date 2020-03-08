9d299e87b6d0f3149c87ad1053d24538de4be248
Naman Shrimali <namanshrimali@gmail.com>
2020-03-09 00:19:33 +0530

    added angular material as a module, added dependencies

---
+++ ui/spry-app/src/app/material/material.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button'; 
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    MatCardModule,
+    MatSelectModule,
+    MatButtonModule
+  ]
+})
+export class MaterialModule { }
